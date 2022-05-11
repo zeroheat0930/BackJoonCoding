d721b3cc0003c6e1131a250160ccb1da08a660b4
zeroheat0930 <80299089+zeroheat0930@users.noreply.github.com>
2022-05-11 22:48:43 +0900

    10869 - 사칙연산

---
+++ src/ez009_10869.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class ez009_10869 {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int A = in.nextInt();
+		int B = in.nextInt();
+ 
+        in.close();
+ 
+		System.out.println(A+B);
+		System.out.println(A-B);
+		System.out.println(A*B);
+		System.out.println(A/B);
+		System.out.println(A%B);
+	}
+}
