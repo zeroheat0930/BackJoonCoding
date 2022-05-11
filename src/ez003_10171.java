760987d2d5212087634b3e169369c7a502c7248b
zeroheat0930 <80299089+zeroheat0930@users.noreply.github.com>
2022-05-11 22:42:19 +0900

    10171 - 고양이

--- src/ez003_10171.java
+++ src/ez003_10171.java
@@ -1,4 +1,9 @@
 
 public class ez003_10171 {
-
+    public static void main(String[] args){
+     	System.out.println( "\\    /\\ \n" + 
+			" )  ( ') \n" + 
+			"(  /  )\n" + 
+			" \\(__)|");
+}
 }
