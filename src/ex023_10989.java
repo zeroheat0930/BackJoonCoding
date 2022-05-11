e103ce39b51e54c5f951990ca96fa902b01756ae
zeroheat0930 <80299089+zeroheat0930@users.noreply.github.com>
2022-05-11 22:41:23 +0900

    c1

--- src/ex023_10989.java
+++ src/ex023_10989.java
@@ -1,4 +1,18 @@
+import java.util.Scanner;
 
 public class ex023_10989 {
-
-}
+		public static void main(String[] args) {
+	    
+			Scanner in = new Scanner(System.in);
+			
+			int a = in.nextInt();
+			int s = 0;
+			
+			
+			for(int i = 1; i <= a; i++) {
+				s += i;
+				
+			}
+			System.out.println(s);
+		}
+	}
