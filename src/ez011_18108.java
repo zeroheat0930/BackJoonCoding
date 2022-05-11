d89ad92aa4137178966bad8924630b09366a4fc0
zeroheat0930 <80299089+zeroheat0930@users.noreply.github.com>
2022-05-11 22:53:18 +0900

    18108 - 1998년생인 내가 태국에서는 2541년생?!

---
+++ src/ez011_18108.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class ez011_18108 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.print(n-543);
+    }
+}
