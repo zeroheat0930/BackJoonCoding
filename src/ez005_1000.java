b0775d66b405675b315ff12edd0e50a183698128
zeroheat0930 <80299089+zeroheat0930@users.noreply.github.com>
2022-05-11 22:44:48 +0900

    1000 - A + B

--- src/ez005_1000.java
+++ src/ez005_1000.java
@@ -1,4 +1,15 @@
+import java.util.Scanner;
 
 public class ez005_1000 {
-
-}
+	 
+		public static void main(String[] args) {
+	 
+			Scanner in = new Scanner(System.in);
+			int A = in.nextInt();
+			int B = in.nextInt();
+			
+			System.out.println(A+B);
+	 
+			in.close();
+		}
+	}
