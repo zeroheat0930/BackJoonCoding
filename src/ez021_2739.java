7c7b2cdd82a6364153f4b74c65d4bc9e159820ff
zeroheat0930 <80299089+zeroheat0930@users.noreply.github.com>
2022-05-11 23:25:05 +0900

    2739 - 구구단

---
+++ src/ez021_2739.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class ez021_2739 {
+	public static void main(String[] args) {
+	    
+		Scanner in = new Scanner(System.in);
+		
+		int a = in.nextInt();
+		
+		for(int i = 1 ; i < 10; i++) {
+			System.out.println(a + " * " + i + " = " + (a*i));
+		}
+	}
+}
