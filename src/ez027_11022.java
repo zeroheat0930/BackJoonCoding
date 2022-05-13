eeb89aa8d2eb012cdb6a5be379951c35c77780d8
zeroheat0930 <80299089+zeroheat0930@users.noreply.github.com>
2022-05-13 22:30:47 +0900

    11022 - A+B -8

---
+++ src/ez027_11022.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class ez027_11022 {
+public static void main(String[] args) throws IOException {
+
+	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	
+	int N = Integer.parseInt(br.readLine());
+	int A;
+	int B;
+	
+	StringTokenizer st;
+	
+	for(int i = 1 ; i <= N; i++) {
+		st = new StringTokenizer(br.readLine()," ");
+		A = Integer.parseInt(st.nextToken());
+		B = Integer.parseInt(st.nextToken());
+		System.out.println("Case #" + i + ": "+A+" + "+B + " = " + (A+B));
+	}
+	
+}
+}
