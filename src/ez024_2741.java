c1069439ad7ff47e8d5180d4a999ddce74768798
zeroheat0930 <80299089+zeroheat0930@users.noreply.github.com>
2022-05-13 20:29:18 +0900

    2741 - N찍기

---
+++ src/ez024_2741.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class ez024_2741 {
+public static void main(String[] args) {
+	Scanner in = new Scanner(System.in);
+	
+	int A = in.nextInt();
+	
+	for(int i = 1; i<= A; i++) {
+		System.out.println(i);
+	}
+}
+}
