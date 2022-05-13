81e4ec364b7fd0b49f56970912fc4cc32550cd87
zeroheat0930 <80299089+zeroheat0930@users.noreply.github.com>
2022-05-13 20:33:47 +0900

    2742 - 기찍 N

---
+++ src/ez025_2742.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class ez025_2742 {
+public static void main(String[] args) {
+	Scanner in = new Scanner(System.in);
+	
+	int A = in.nextInt();
+	
+	for(int i = A; i>= 1; i--) {
+		System.out.println(i);
+	}
+}
+}
