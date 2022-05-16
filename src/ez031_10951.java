60c015a214a6776789a5d6d34018763dfbeb51e7
zeroheat0930 <80299089+zeroheat0930@users.noreply.github.com>
2022-05-16 20:20:18 +0900

    10951 - A+B - 4

---
+++ src/ez031_10951.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class ez031_10951 {
+	public static void main(String args[]) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+		String str;
+ 
+		while( (str=br.readLine()) != null ){
+		    
+			st = new StringTokenizer(str," ");
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			sb.append(a+b).append("\n");
+		
+		}
+		System.out.print(sb);
+	}
+}
